80968f0f0ba1eb6e28e354055f9e4dc8689bc8a9
jehadkalifah <jehad.khalifaah@gmail.com>
2019-11-26 16:08:51 +0200

    docker-compose

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,7 @@
 # pushing the images to Docker Hub
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+docker build -t $DOCKER_WEB_IMAGE /web
+docker build -t $DOCKER_NGINX_IMAGE /nginx
 docker push $DOCKER_WEB_IMAGE
 docker push $DOCKER_NGINX_IMAGE
 
@@ -8,7 +10,7 @@ docker-compose up -d
 docker ps
 docker-compose down 
 
-
+# deploy the environment to Azure Kubernetes Service
 #sudo az login --service-principal -u $AZURE_SP_USERNAME -p $AZURE_SP_PASSWORD --tenant $AZURE_SP_TENANT
 #sudo az aks get-credentials --resource-group $AZURE_AKS_RESOURCE_GROUP --name $AZURE_AKS_NAME
 #sudo kubectl apply -f ./kube
