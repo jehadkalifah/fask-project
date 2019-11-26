ead605949fe006d8bf18f4ae65cbfddb05d6c380
jehadkalifah <jehad.khalifaah@gmail.com>
2019-11-26 14:05:48 +0200

    docker-compose

---
+++ scripts/deploy.sh
@@ -0,0 +1,15 @@
+# pushing the images to Docker Hub
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+docker push $DOCKER_WEB_IMAGE
+docker push $DOCKER_NGINX_IMAGE
+
+# testing the environment
+docker-compose up -d
+docker ps
+docker-compose down 
+
+
+#sudo az login --service-principal -u $AZURE_SP_USERNAME -p $AZURE_SP_PASSWORD --tenant $AZURE_SP_TENANT
+#sudo az aks get-credentials --resource-group $AZURE_AKS_RESOURCE_GROUP --name $AZURE_AKS_NAME
+#sudo kubectl apply -f ./kube
+#sudo kubectl get all -n fask-namespace
