b6ae0c988a9ac2e3c8afe7313baeb9835525de9a
jehadkalifah <jehad.khalifaah@gmail.com>
2019-11-26 09:39:01 +0200

    my project

--- web/app/app.py
+++ web/app/app.py
@@ -21,7 +21,7 @@ from redis import Redis
 
 
 app = Flask(__name__)
-redis = Redis(host=os.environ.get('REDIS_HOST', 'redis'), port=6379)
+redis = Redis(host=os.environ.get('REDIS_HOST'), port=6379)
 
 
 @app.route('/')
